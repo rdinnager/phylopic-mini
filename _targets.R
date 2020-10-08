26b3a01bc2833a490889f1f1abe1c964cbb09dc2
rdinnager <r.dinnage@gmail.com>
2020-10-09 00:04:20 +1100

    okay, got a system going for checking all phylopic pngs for updates before downloading. looks like it is working but I have to say it is slow.

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ source("R/functions.R")
 
 # Set target-specific options such as packages.
 options(tidyverse.quiet = TRUE)
-tar_option_set(packages = c("dplyr", "rphylopic", "tidyr"))
+tar_option_set(packages = c("dplyr", "rphylopic", "tidyr", "purrr"))
 
 # Define targets
 targets <- list(
@@ -20,7 +20,24 @@ targets <- list(
              rphylopic::image_list(length = phylopic_count, 
                                    options=c('credit','licenseURL','pngFiles','submitted','submitter',
                                              'svgFile','taxa','canonicalName','string','uri','type',
-                                             'citationStart', 'directNames')))
+                                             'citationStart', 'directNames'))),
+  
+  tar_target(max_res, map_int(pic_data, ~length(.x$pngFiles))),
+  
+  tar_target(png_urls, map2_chr(pic_data, max_res,
+                                ~.x$pngFiles[[.y]]$url)),
+  
+  tar_target(png_files, paste0("http://phylopic.org", png_urls), format = "url", 
+             pattern = map(png_urls)),
+  
+  tar_target(local_files, file.path("data/phylopic/pngs", basename(png_urls)), format = "file",
+             pattern = map(png_files)),
+  
+  # tar_target(pngs, download_phylopics(png_files, local_files), format = "file", 
+  #            pattern = map(png_files, local_files)),
+  
+  NULL
+  
 )
 
 # End with a call to tar_pipeline() to wrangle the targets together.
